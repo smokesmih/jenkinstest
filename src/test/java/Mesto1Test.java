6416ac0a133350fa8740cff70bb4b4853950ecd4
Mikhail Katin <mihailkatin7@gmail.com>
2024-09-07 19:01:41 +0500

    2 commit

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -65,5 +65,14 @@ public class Mesto1Test {
                 .delete("/api/cards/{photoId}/likes", photoId) // Делаем DELETE-запрос
                 .then().assertThat().statusCode(200); // Проверяем, что сервер вернул код 200
     }
+    @Test
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
+    }
 
 }
